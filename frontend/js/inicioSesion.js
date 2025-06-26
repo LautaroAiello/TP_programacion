76918f4b88511fd80af086c8ca52ecfedb774ded
lautaro <lautaroaiello34@gmail.com>
2025-06-26 17:53:55 -0300

    arreglo de estilos

--- frontend/js/inicioSesion.js
+++ frontend/js/inicioSesion.js
@@ -2,6 +2,28 @@ const formularioInicio = document.getElementById("formularioInicioSesion");
 const loginDiv = document.getElementById("login");
 //const textoSesiones = document.getElementById("textoSesiones");
 
+// const msjEmail = document.getElementById("msjErrorEmail");
+// const msjPsw = document.getElementById("pswErrorEmail");
+
+// email.addEventListener("blur", () =>{
+//     if(email.value == ""){
+//         msjEmail.style.display = "block";
+//         msjEmail.innerText = "Por favor escriba su e-mail";
+//     }else{
+//         msjEmail.style.display = "none";
+//         msjEmail.innerText = "";
+//     }
+// })
+// password.addEventListener("blur", () =>{
+//     if(password.value == ""){
+//         msjPsw.style.display = "block";
+//         msjPsw.innerText = "Por favor escriba su contraseña";
+//     }else{
+//         msjPsw.style.display = "none";
+//         msjPsw.innerText = "";
+//     }
+// })
+
 formularioInicio.addEventListener('submit',async(e)=>{
     e.preventDefault();
     const email = document.getElementById("emailInicioSesion").value;
@@ -18,11 +40,12 @@ formularioInicio.addEventListener('submit',async(e)=>{
         .then(data=>{
             if(data.payload.length != 0){
                 console.log(data);
-                inicioSesionExitosa(data.payload[0].nombre);
                 guardarInicioLocalStorage("authToken",data.jwt);
                 guardarInicioLocalStorage("id",data.payload[0].id_usuario)
+                window.location.reload();
+                inicioSesionExitosa(data.payload[0].nombre);
                 setTimeout(()=>{
-                    window.location.reload();
+                   loginDiv.style.display = "none";
                 },1000)
             }else{
                 let mensajeError = document.getElementById("mensajeError");
