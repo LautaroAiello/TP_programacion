47fd4b8ee3d6aa15b09b51fe54d5b60588c506f3
lautaro <lautaroaiello34@gmail.com>
2025-06-28 03:53:31 -0300

    funcionesAdmin de agregar categoria,producto e inventario finalizadas

--- frontend/js/agregarProductos.js
+++ frontend/js/agregarProductos.js
@@ -1,4 +1,4 @@
-// POST - /api/cargarProducto
+// .POST - /api/cargarProducto
 //         body:
 //         {
 //             nombre: string
@@ -9,18 +9,92 @@
 //             imagen: string
 
 //         }
+// . POST - /api/crearInventario
+//         body: 
+//         {
+//             talle: string
+//             color: string
+//             stock: int
+//             id_producto: int
+//         }
+// . POST - /api/crearCategoria
+//         body: 
+//         {
+//             nombre: string
+//         }
 
 const formularioRegistroProducto = document.getElementById("editarFormAdmin");
+const formularioCategoria = document.getElementById("formCategoria");
+const formularioInventario = document.getElementById("formInventario");
 let tokenAuth = localStorage.getItem('authToken');
 
 
-formularioRegistroProducto.addEventListener('submit', async (e)=>{
+
+formularioCategoria.addEventListener("submit", async (e) =>{ // este formulario crea una categoria
+    e.preventDefault();
+    let nombre= document.getElementById("categoria").value;
+    try {
+        const crearCategoria = await fetch('http://localhost:4000/api/crearCategoria',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json',
+                Authorization: `${tokenAuth}`
+            },
+            body: JSON.stringify({nombre}) 
+        });
+        if(!crearCategoria.ok){
+            throw new Error("Error en la solicitud.");
+        }
+        const nombreCategoria = await crearCategoria.json();
+        console.log("Categoria cargada correctamente.", nombreCategoria);
+        document.getElementById("mensajeExitoCategoria").style.display = "block";
+        setTimeout(()=> {
+            document.getElementById("mensajeExitoCategoria").style.display = "none";
+            window.location.reload();
+        },2000)
+        
+    } catch (error) {
+        console.error(error)
+        alert("Error al crear categoria.")
+    }
+})
+
+
+async function cargarIDCategorias(){ //Esta función carga las categorías en el select del formulario de registro de productos
+    try {
+        const respuesta = await fetch("http://localhost:4000/api/obtenerCategorias", {
+            headers: {
+                Authorization: `${tokenAuth}`
+            }
+        });
+
+        if (!respuesta.ok){
+          throw new Error("Error al obtener categorías");  
+        } 
+        const objeto = await respuesta.json();
+        const categorias = objeto.payload;
+        const selectCategoria = document.getElementById("categoriaSelect");
+        categorias.forEach(cat => {
+            selectCategoria.innerHTML += `<option value="${cat.id_categoria}" class="dataUsuario" >${cat.nombre}</option>` 
+        });
+    } catch (error) {
+        console.error("Error al cargar categorías:", error);
+        alert("No se pudieron cargar las categorías.");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    cargarIDCategorias();
+});
+
+
+formularioRegistroProducto.addEventListener('submit', async (e)=>{ //este formulario carga un producto
     e.preventDefault();   
         let nombre = document.getElementById("nombre").value;
         let descripcion = document.getElementById("descripcion").value;
         let precio = parseInt(document.getElementById("precio").value);
         let genero = document.getElementById("genero").value;
-        let id_categoria = parseInt(document.getElementById("categoria").value);
+        let id_categoria = parseInt(document.getElementById("categoriaSelect").value);
         let imagen = document.getElementById("imagen").value;
         try {
             const cargarProducto = await fetch('http://localhost:4000/api/cargarProducto', {
@@ -36,7 +110,11 @@ formularioRegistroProducto.addEventListener('submit', async (e)=>{
             }
             const resultado = await cargarProducto.json();
             console.log("Producto cargado correctamente.", resultado);
-            // MENSAJE DE CARGA EXITOSA
+            document.getElementById("mensajeExitoProducto").style.display = "block";
+            setTimeout(()=> {
+                document.getElementById("mensajeExitoProducto").style.display = "none";
+                window.location.reload();
+            },2000)
 
         } catch (error) {
             console.error(error);
@@ -47,3 +125,64 @@ formularioRegistroProducto.addEventListener('submit', async (e)=>{
 
 })
 
+
+async function cargarIDproductos(){  //Esta función carga los productos en el select del formulario de inventario
+    try {
+        const respuesta = await fetch("http://localhost:4000/api/obtenerProductos", {
+            headers: {
+                Authorization: `${tokenAuth}`
+            }
+        });
+
+        if (!respuesta.ok){
+          throw new Error("Error al obtener categorías");  
+        } 
+        const objeto = await respuesta.json();
+        const productos = objeto.payload;
+        const selectIDProducto = document.getElementById("productoSelect");
+        productos.forEach(p => {
+            selectIDProducto.innerHTML += `<option value="${p.idProducto}" class="dataUsuario" >${p.producto}</option>` 
+        });
+    } catch (error) {
+        console.error("Error al cargar categorías:", error);
+        alert("No se pudieron cargar las categorías.");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    cargarIDproductos();
+});
+
+formularioInventario.addEventListener("submit", async (e) => { // este formulario carga el inventario de un producto
+    e.preventDefault();
+    let talle = document.querySelector('[name="talles"]:checked').value;
+    let color = document.getElementById("color").value;
+    let stock = parseInt(document.getElementById("stock").value);
+    let id_producto = parseInt(document.getElementById("productoSelect").value);
+
+    try {
+        const cargarInventario = await fetch('http://localhost:4000/api/crearInventario',{
+            method: 'POST',
+                headers:{
+                    'Content-type' : 'application/json',
+                    Authorization: `${tokenAuth}`                                       
+                },
+            body: JSON.stringify({talle,color,stock,id_producto})
+        });
+        if(!cargarInventario.ok){
+            const errText = await cargarInventario.text();
+            throw new Error("Error al crear inventario: " + errText);
+        }
+        const inventario = await cargarInventario.json();
+        console.log("Inventario creado con exito.",inventario);
+        document.getElementById("mensajeExitoInventario").style.display = "block";
+        setTimeout(()=> {
+            document.getElementById("mensajeExitoInventario").style.display = "none";
+            window.location.reload();
+        },2000)
+        
+    } catch (error) {
+        console.error(error)
+        alert("Error al crear inventario.")
+    }
+})
